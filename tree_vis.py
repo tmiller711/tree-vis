9573316cc7db5bcb575bf7ea89f0fdce9e8e3f45
Tristin <tmille2004@gmail.com>
2022-12-25 09:57:37 -0600

    creates connect_nodes function and build_tree function

--- tree_vis.py
+++ tree_vis.py
@@ -1,6 +1,6 @@
 import pygame
 
-WIDTH, HEIGHT = 750, 450
+WIDTH, HEIGHT = 900, 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Tree Visualization")
 FPS = 60
@@ -21,32 +21,48 @@ class TreeNode:
         self.y = y
         self.left = None
         self.right = None
+        self.color = WHITE
 
     def draw_node(self):
-        # x = WIDTH//2
-        # y = 50
-        pygame.draw.circle(WIN, WHITE, (self.x, self.y), self.radius, 1)
+        pygame.draw.circle(WIN, self.color, (self.x, self.y), self.radius, 1)
 
-        text_surface = font.render(self.val, 1, WHITE)
+        text_surface = font.render(self.val, 1, self.color)
         text_rect = text_surface.get_rect()
         text_rect.center = (self.x, self.y)
 
         WIN.blit(text_surface, text_rect)
 
-def draw_window(nodes):
+def build_tree():
+    root = TreeNode("5", WIDTH//2, 50)
+    root.left = TreeNode("8", WIDTH//2-75, 125)
+    root.right = TreeNode("3", WIDTH//2+75, 125)
+
+    root.left.left = TreeNode("9", WIDTH//2-150, 200)
+    root.left.right = TreeNode("12", WIDTH//2-100, 200)
+
+    return root
+
+def connect_nodes(node1, node2):
+    line_start = (node1.x, node1.y+node1.radius)
+    line_end = (node2.x, node2.y-node2.radius)
+    line_color = WHITE
+    pygame.draw.line(WIN, line_color, line_start, line_end, 1)
+
+def draw_window(root):
     WIN.fill(BLACK)
-    for node in nodes:
-        node.draw_node()
 
+    queue = [root]
 
-    # Set the line start and end points
-    # line_start = (WIDTH//2, 50 + 30)
-    # line_end = (100, 100)
+    while queue:
+        current = queue.pop()
+        current.draw_node()
+        if current.left:
+            connect_nodes(current, current.left)
+            queue.append(current.left)
+        if current.right:
+            connect_nodes(current, current.right)
+            queue.append(current.right)
 
-    # Set the line color
-    # line_color = WHITE
-
-    # pygame.draw.line(WIN, line_color, line_start, line_end, 1)
     pygame.display.update()
 
 def main():
@@ -60,11 +76,9 @@ def main():
                 run = False
                 pygame.quit()
 
-        node1 = TreeNode("5", WIDTH//2, 50)
-        node2 = TreeNode("8", WIDTH//2-50, 100)
-        node3 = TreeNode("3", WIDTH//2+50, 100)
+        root = build_tree()
 
-        draw_window([node1, node2, node3])
+        draw_window(root)
 
     main()
 
